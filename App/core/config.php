b20439713544a6d2fca74c82effcc619c337bcb2
Amine Es-saouiqui <amineessa02@gmail.com>
2024-08-06 14:53:22 +0100

    Finished

--- App/core/config.php
+++ App/core/config.php
@@ -2,7 +2,7 @@
 /**
  * @author : Es-saouiqui Amine
 */
-define('DBHOST', 'localhost');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBNAME', 'ESERVICE');
+define('DBHOST', 'sql307.infinityfree.com');
+define('DBUSER', 'if0_37047795');
+define('DBPASS', '9GC1luucbpI');
+define('DBNAME', 'if0_37047795_eservice');
